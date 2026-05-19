4f446a0237f8b439a94774d254911a36812d5798
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2026-05-19 02:24:32 -0400

    Remove tests and extra routes, templates, etc

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Laravel\Fortify\Features;
 use App\Data\Method;
 
 Route::inertia('/', 'welcome')->name('home');
 
 Route::inertia('/docs', 'docs', [ 'methods' => resolve(Method::class) ])->name('docs');
-// Route::middleware(['auth', 'verified'])->group(function () {
-//     Route::inertia('dashboard', 'dashboard')->name('dashboard');
-// });
-
-require __DIR__.'/settings.php';
